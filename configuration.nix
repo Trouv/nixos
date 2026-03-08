77ee4e8c60c7cb2e6a4fc8f2a5cb2d9086f300b7
Trevor Lovell <trevorlovelldesign@gmail.com>
2026-03-07 19:47:29 -0700

    feat: change hostname to pangolin

--- configuration.nix
+++ configuration.nix
@@ -17,7 +17,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   boot.initrd.luks.devices."luks-e72a7a07-20ec-444d-a711-c693cf9ed082".device = "/dev/disk/by-uuid/e72a7a07-20ec-444d-a711-c693cf9ed082";
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "pangolin"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
