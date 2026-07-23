852d4c076fc0d3f5fa0870d65cfb96f9cb1cd5ec
Trevor Lovell <trevorlovelldesign@gmail.com>
2026-07-22 23:59:16 -0600

    feat: add hardware setting for GPU

--- configuration.nix
+++ configuration.nix
@@ -140,6 +140,12 @@
   # enables waybar power profiles module
   services.power-profiles-daemon.enable = true;
 
+  # AMD GPU
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
