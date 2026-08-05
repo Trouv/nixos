e7a21be727f2f8b627014dec99775698d749a749
Trevor Lovell <trevorlovelldesign@gmail.com>
2026-08-04 22:15:22 -0600

    fix: discord screencast

--- configuration.nix
+++ configuration.nix
@@ -33,6 +33,28 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
   environment.pathsToLink = ["/share/applications" "/share/xdg-desktop-portal"];
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    config = {
+      common = {
+        default = [
+          "gtk"
+          "gnome"
+        ];
+      };
+      niri = {
+        default = [
+          "gtk"
+          "gnome"
+        ];
+      };
+    };
+  };
+  xdg.portal.extraPortals = [
+    pkgs.xdg-desktop-portal-gtk
+    pkgs.xdg-desktop-portal-gnome
+  ];
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -78,7 +100,7 @@
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    wireplumber.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
