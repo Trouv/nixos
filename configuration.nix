45c507f70cbf3b47eeb1a589292980cd60f0ebb2
Trevor Lovell <trevorlovelldesign@gmail.com>
2026-08-06 19:31:38 -0600

    feat: split some things into preConfiguration

--- configuration.nix
+++ configuration.nix
@@ -4,30 +4,16 @@
 {
   pkgs,
   inputs,
-  systemSettings,
   ...
 }: {
   imports = [
     # Include the results of the hardware scan.
-    systemSettings.hardware-configuration
     inputs.niri.nixosModules.niri
-    ./cachix.nix
+    ./preConfiguration.nix
   ];
 
   nixpkgs.overlays = [inputs.niri.overlays.niri];
   programs.niri.enable = true;
-
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  boot.initrd.luks.devices."${systemSettings.luksDeviceName}".device = systemSettings.luksDevicePath;
-  networking.hostName = systemSettings.hostName; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -56,27 +42,6 @@
     pkgs.xdg-desktop-portal-gnome
   ];
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "America/Denver";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
@@ -101,19 +66,6 @@
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     wireplumber.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.trouv = {
-    isNormalUser = true;
-    description = "Trevor Lovell";
-    extraGroups = ["networkmanager" "wheel" "input"];
-    packages = with pkgs; [
-      #  thunderbird
-    ];
   };
 
   # Allow unfree packages
@@ -181,38 +133,10 @@
 
     # temperature sensors
     lm_sensors
-
-    # extra binary cache
-    cachix
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # enables waybar power profiles module
   services.power-profiles-daemon.enable = true;
-
-  # AMD GPU
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
 
   stylix = {
     enable = true;
@@ -261,16 +185,4 @@
       User = "trouv";
     };
   };
-
-  # The default resolv.conf is buggy on my personal network
-  # Letting systemd-resolved manage it seems to work around the issue.
-  services.resolved.enable = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
 }
---
+++ preConfiguration.nix
@@ -0,0 +1,83 @@
+# This file provides a significant portion of the system configuration.
+# In particular, it provides the portion of the system configuration that is
+# required for basic function AND improves the nixos-rebuild experience.
+# i.e., anything that makes the build faster, or does away with extra
+# nixos-rebuild arguments.
+{
+  pkgs,
+  systemSettings,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    systemSettings.hardware-configuration
+    ./cachix.nix
+  ];
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  boot.initrd.luks.devices."${systemSettings.luksDeviceName}".device = systemSettings.luksDevicePath;
+  networking.hostName = systemSettings.hostName;
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Denver";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.trouv = {
+    isNormalUser = true;
+    description = "Trevor Lovell";
+    extraGroups = ["networkmanager" "wheel" "input"];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # extra binary cache
+    cachix
+  ];
+
+  # AMD GPU
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  # The default resolv.conf is buggy on my personal network
+  # Letting systemd-resolved manage it seems to work around the issue.
+  services.resolved.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+}
