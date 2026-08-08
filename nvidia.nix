77949a50a927c06c1f859feb90de54ca4d1684c3
Trevor Lovell <trevorlovelldesign@gmail.com>
2026-08-07 18:05:54 -0600

    feat: define more hardware.nvidia values

--- nvidia.nix
+++ nvidia.nix
@@ -7,5 +7,18 @@
 
   config = lib.mkIf config.nvidia.enable {
     services.xserver.videoDrivers = ["nvidia"];
+
+    hardware.nvidia = {
+      modesetting.enable = true;
+
+      powerManagement.enable = false;
+      powerManagement.finegrained = false;
+
+      open = true;
+
+      nvidiaSettings = true;
+
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
   };
 }
