49cc210fedb05738b8a83eb8c936e98c7ed9e36f
Trevor Lovell <trevorlovelldesign@gmail.com>
2026-08-08 22:26:36 -0600

    refactor: move kopia connection sh to its own script

--- home/trouv/programs/niri/default.nix
+++ home/trouv/programs/niri/default.nix
@@ -30,13 +30,7 @@
     spawn-at-startup = [
       {argv = ["waybar"];}
       {
-        sh = ''
-          kitty \
-            --hold \
-            kopia repository connect server \
-              --url https://192.168.0.4:51515/ \
-              --server-cert-fingerprint 73fa21b188d22957ed2a6635e1fb994ff6fd9546d02e0e14f2469cf5a0eb175a
-        '';
+        sh = "kitty --hold kopia-connect-vatia";
       }
     ];
 
---
+++ scripts/kopia-connect-vatia.nix
@@ -0,0 +1,5 @@
+''
+  kopia repository connect server \
+    --url https://192.168.0.4:51515/ \
+    --server-cert-fingerprint 73fa21b188d22957ed2a6635e1fb994ff6fd9546d02e0e14f2469cf5a0eb175a
+''
